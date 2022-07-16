747199407054302f4459be24481df8726ed02523
Steven Good <steve.good.154@gmail.com>
2022-07-16 10:33:50 -0400

    Tile based player movement

--- Help-Your-Selves/Assets/_Scripts/Player.cs
+++ Help-Your-Selves/Assets/_Scripts/Player.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    // Start is called before the first frame update
-
-    private void Awake() {
+    
+    private void Awake()
+    {
         this.rb = GetComponent<Rigidbody2D>();
-        // this.rb.velocity = new Vector2(-1 , this.rb.velocity.y);
     }
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -19,15 +20,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int x = (int) this.rb.velocity.x;
-        int y = (int) this.rb.velocity.y;
-        if(Input.GetKeyDown("left")) x = -1;
-        else if(Input.GetKeyDown("right")) x = 1;
-        if(Input.GetKeyDown("down")) y = -1;
-        else if(Input.GetKeyDown("up")) y = 1;
-        
-        this.rb.velocity = new Vector2(x , y);
-        
-        
+        Vector3 vec = new Vector3(0f, 0f, 0f);
+        if (Input.GetKeyDown("right"))
+        {
+            vec.x = 1f;
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+           vec.x = -1f;
+        }
+        else if (Input.GetKeyDown("up"))
+        {
+            vec.y = 1f;      }
+
+        else if (Input.GetKeyDown("down"))
+        {
+            vec.y = -1f;
+        }
+        this.transform.position += vec;
     }
 }
