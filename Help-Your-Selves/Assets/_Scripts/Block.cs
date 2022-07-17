e4b7e7b13560b9bb29bbe7f8c4562e17751d796c
jlpeters <jlpeters@sep.com>
2022-07-17 13:44:33 -0400

    Added color changing to block

--- Help-Your-Selves/Assets/_Scripts/Block.cs
+++ Help-Your-Selves/Assets/_Scripts/Block.cs
@@ -45,6 +45,8 @@ public class Block : MonoBehaviour, IBlock
 
     public void setColor(int color){
         this.color = color;
+        SpriteRenderer sprite = this.GetComponent<SpriteRenderer>();
+        sprite.color = Colors.getColorById(color);
     }
 
 }
